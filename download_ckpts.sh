5fbcfb418b95d9c3a1b68b0d5fbc770becbd842d
Oliver-Cong02 <xiaoyan_cong@brown.edu>
2026-01-10 15:34:25 -0500

    update download ckpt

---
+++ download_ckpts.sh
@@ -0,0 +1,4 @@
+huggingface-cli download tencent/HY-Motion-1.0 --include "HY-Motion-1.0-Lite/*" --local-dir ckpts/tencent
+huggingface-cli download openai/clip-vit-large-patch14 --local-dir ckpts/clip-vit-large-patch14/
+huggingface-cli download Qwen/Qwen3-8B --local-dir ckpts/Qwen3-8B
+huggingface-cli download Text2MotionPrompter/Text2MotionPrompter --local-dir ckpts/Text2MotionPrompter
